b6d4b2f933bc9f82360a3a665c4aa023e6540c96
hirokuma <hiro9ma@gmail.com>
2017-10-26 00:53:18 +0900

    read public crt

--- app/src/main/java/com/blogpost/hiro99ma/nfc/NfcFactory.java
+++ app/src/main/java/com/blogpost/hiro99ma/nfc/NfcFactory.java
@@ -15,9 +15,9 @@ import android.util.Log;
  */
 public class NfcFactory {
 
-	private final static String TAG = "NfcFactory";
+    private final static String TAG = "NfcFactory";
 
-	private final static int READ_TAG_TYPE = NfcAdapter.FLAG_READER_NFC_B | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
+    private final static int READ_TAG_TYPE = NfcAdapter.FLAG_READER_NFC_B | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
 
     /**
      * onResume()時の動作
@@ -26,43 +26,62 @@ public class NfcFactory {
      * @return				true:NFCタグ検出の準備ができた<br />
      * 						false:できなかった
      */
-	public static boolean nfcResume(Activity activity, NfcAdapter.ReaderCallback callback) {
-		//NFC
-		NfcManager mng = (NfcManager)activity.getSystemService(Context.NFC_SERVICE);
-		if (mng == null) {
-			Log.e(TAG, "no NfcManager");
-			return false;
-		}
-		NfcAdapter adapter = mng.getDefaultAdapter();
-		if (adapter == null) {
-			Log.e(TAG, "no NfcService");
-			return false;
-		}
+    public static boolean nfcResume(Activity activity, NfcAdapter.ReaderCallback callback) {
+        //NFC
+        NfcManager mng = (NfcManager)activity.getSystemService(Context.NFC_SERVICE);
+        if (mng == null) {
+            Log.e(TAG, "no NfcManager");
+            return false;
+        }
+        NfcAdapter adapter = mng.getDefaultAdapter();
+        if (adapter == null) {
+            Log.e(TAG, "no NfcService");
+            return false;
+        }
 
-		adapter.enableReaderMode(activity, callback, READ_TAG_TYPE, null);
+        adapter.enableReaderMode(activity, callback, READ_TAG_TYPE, null);
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * onPause()時の動作
-	 *
-	 * @param activity		現在のActivity。だいたいthisを渡すことになる。
-	 */
-	public static void nfcPause(Activity activity) {
-		NfcManager mng = (NfcManager)activity.getSystemService(Context.NFC_SERVICE);
-		if (mng == null) {
-			Log.e(TAG, "no NfcManager");
-			return;
-		}
-		NfcAdapter adapter = mng.getDefaultAdapter();
-		if (adapter == null) {
-			Log.e(TAG, "no NfcService");
-			return;
-		}
+    /**
+     * onPause()時の動作
+     *
+     * @param activity		現在のActivity。だいたいthisを渡すことになる。
+     */
+    public static void nfcPause(Activity activity) {
+        NfcManager mng = (NfcManager)activity.getSystemService(Context.NFC_SERVICE);
+        if (mng == null) {
+            Log.e(TAG, "no NfcManager");
+            return;
+        }
+        NfcAdapter adapter = mng.getDefaultAdapter();
+        if (adapter == null) {
+            Log.e(TAG, "no NfcService");
+            return;
+        }
 
-		if (activity.isFinishing()) {
-			adapter.disableReaderMode(activity);
-		}
-	}
+        if (activity.isFinishing()) {
+            adapter.disableReaderMode(activity);
+        }
+    }
+
+    public static byte[] isoDepCase2Short(byte ins, byte p1, byte p2, byte le) {
+        return new byte[] { 0x00, ins, p1, p2, le };
+    }
+
+    public static byte[] isoDepCase3Short(byte ins, byte p1, byte p2, byte[] data) {
+        byte[] result = new byte[5 + data.length];
+        result[0] = 0x00;		//CLA
+        result[1] = ins;
+        result[2] = p1;
+        result[3] = p2;
+        result[4] = (byte)data.length;
+        System.arraycopy(data, 0, result, 5, data.length);
+        return result;
+    }
+
+    public static boolean isoDepCheckResult(byte[] result) {
+        return (result.length >= 2) && (result[result.length - 2] == (byte) 0x90) && (result[result.length - 1] == 0x00);
+    }
 }
--- app/src/main/java/com/blogpost/hiro99ma/readjpkpubkey/MainActivity.java
+++ app/src/main/java/com/blogpost/hiro99ma/readjpkpubkey/MainActivity.java
@@ -3,13 +3,21 @@ package com.blogpost.hiro99ma.readjpkpubkey;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.nfc.tech.NfcB;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.blogpost.hiro99ma.nfc.NfcFactory;
 
+import java.io.IOException;
+import java.nio.Buffer;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +43,68 @@ public class MainActivity extends AppCompatActivity {
     private NfcAdapter.ReaderCallback mCallBack = new NfcAdapter.ReaderCallback() {
         @Override
         public void onTagDiscovered(Tag tag) {
-            runOnUiThread(new Runnable() {
-                  @Override
-                  public void run() {
-                      Toast.makeText(MainActivity.this, android.R.string.ok, Toast.LENGTH_SHORT).show();
-                  }
-            });
+            if (NfcB.get(tag) != null) {
+                //
+                IsoDep nfc = IsoDep.get(tag);
+                try {
+                    byte[] command;
+                    byte[] result;
+                    nfc.connect();
+
+                    command = NfcFactory.isoDepCase3Short((byte)0xa4, (byte)0x04, (byte)0x0c, new byte[] { (byte)0xD3, (byte)0x92, (byte)0xF0, 0x00, 0x26, 0x01, 0x00, 0x00, 0x00, 0x01 });
+                    result = nfc.transceive(command);
+                    if (!NfcFactory.isoDepCheckResult(result)) {
+                        Log.d(TAG, "select file 1: AP");
+                        throw new RuntimeException("fail1");
+                    }
+
+                    command = NfcFactory.isoDepCase3Short((byte)0xa4, (byte)0x02, (byte)0x0c, new byte[] { 0x00, 0x0a });
+                    result = nfc.transceive(command);
+                    if (!NfcFactory.isoDepCheckResult(result)) {
+                        Log.d(TAG, "select file 2: PIN");
+                        throw new RuntimeException("fail2");
+                    }
+
+                    int point = 0;
+                    int less = -1;
+                    byte[] crt = null;
+                    byte p1 = 0x00;
+                    byte p2 = 0x00;
+                    byte next = (byte)0xff;
+                    while (true) {
+                        command = NfcFactory.isoDepCase2Short((byte)0xb0, p1, p2, next);
+                        result = nfc.transceive(command);
+                        if (!NfcFactory.isoDepCheckResult(result)) {
+                            Log.d(TAG, "read binary");
+                            throw new RuntimeException("fail3");
+                        }
+                        if (point == 0) {
+                            less = (result[2] << 8) | result[3] + 4;
+                            crt = new byte[less];
+                        }
+                        System.arraycopy(result, 0, crt, point, result.length - 2);
+                        point += result.length - 2;
+                        less -= result.length - 2;
+                        if (less == 0) {
+                            break;
+                        } else if (less < result.length) {
+                            next = (byte)less;
+                        }
+                        p1 = (byte)((point & 0xff00) >> 8);
+                        p2 = (byte)(point & 0x00ff);
+                    }
+
+                    nfc.close();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, android.R.string.ok, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     };
 }
